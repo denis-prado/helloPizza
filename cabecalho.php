2d256715c7ea1eecd48cc6d5d9c1184d104e3c6c
Denis Prado <denishenrique60@gmail.com>
2019-12-10 22:22:36 -0300

    Páginas cabecalho e rodape criadas

---
+++ cabecalho.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Cabecalho</title>
+	<link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+	<header>
+		<div id="banner">
+			<h1>
+				<img src="img/logo-helloPizza.png" alt="Logo hello pizza" />
+			</h1>
+
+			<nav> 
+				<ul>
+					<!--
+						target="blank" -> Pode ser usada para abrir um link 
+						em uma nova aba.
+					-->
+					<li><a href="index.php">Home</a></li>
+					<li><a href="#">Sobre</a></li>
+					<li><a href="contato.php">Contato</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+</body>
+</html>
---
+++ rodape.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<title>Cabecalho</title>
+	<link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+	<footer>
+		<div id="copyrigth">
+			<a href="#" id="logo-hellopizza">
+				<img src="img/logo-helloPizza.png" alt="logo hello pizza" />
+			</a>
+
+			<small>Todos os direitos reservados &copy 2019</small>
+		</div>
+	</footer>
+</body>
+</html>
